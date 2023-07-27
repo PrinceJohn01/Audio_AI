6f45cde06b6d0a1ae1042122d304f819b55018a6
PrinceJohn01 <princedav748@gmail.com>
2023-07-27 23:12:28 +0100

    UI is Done

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,6 +1,9 @@
 import 'package:audio_ai/constant/pallete.dart';
+import 'package:audio_ai/feature_list.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:speech_to_text/speech_to_text.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,6 +13,41 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final speechToText = SpeechToText();
+  String lastWords = '';
+
+  @override
+  void initState() {
+    super.initState();
+    initSpeechToText();
+  }
+
+  Future<void> initSpeechToText() async{
+     await speechToText.initialize();
+     setState(() {});
+  }
+
+  Future<void> startListening() async {
+    await speechToText.listen(onResult: onSpeechResult);
+    setState(() {});
+  }
+  // listen method.
+  Future<void> stopListening() async {
+    await speechToText.stop();
+    setState(() {});
+  }
+  // This is the callback that the SpeechToText plugin calls when
+  void onSpeechResult(SpeechRecognitionResult result) {
+    setState(() {
+      lastWords = result.recognizedWords;
+    });
+  }
+  @override
+  void dispose() {
+    super.dispose();
+    speechToText.stop();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,23 +56,101 @@ class _HomePageState extends State<HomePage> {
         leading: const Icon(Icons.menu),
         centerTitle: true,
       ),
-      body: Column(
-        children: [
-          Stack(
-            children: [
-              Center(
-                child: Container(
-                  height: 120,
-                  width: 120,
-                  margin: const EdgeInsets.only(top: 6),
-                  decoration: const BoxDecoration(color: Pallete.assistantCircleColor,
-                  shape: BoxShape.circle),
-
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                Center(
+                  child: Container(
+                    height: 120,
+                    width: 120,
+                    margin: const EdgeInsets.only(top: 6),
+                    decoration: const BoxDecoration(
+                        color: Pallete.assistantCircleColor,
+                        shape: BoxShape.circle),
+                  ),
                 ),
-              )
-            ],
-          )
-        ],
+                Container(
+                    height: 123,
+                    decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                            image: AssetImage(
+                                "assets/images/virtualAssistant.png"))))
+              ],
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20,
+                vertical: 10,
+              ),
+              margin:
+                  const EdgeInsets.symmetric(horizontal: 40).copyWith(top: 30),
+              decoration: BoxDecoration(
+                  border: Border.all(color: Pallete.borderColor),
+                  borderRadius: BorderRadius.circular(20)),
+              child: const Padding(
+                padding: EdgeInsets.symmetric(vertical: 10.0),
+                child: Text(
+                  'Hello, How Can I Assist You',
+                  style: TextStyle(
+                      fontFamily: 'Cera Pro',
+                      color: Pallete.mainFontColor,
+                      fontSize: 25),
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(10),
+              alignment: Alignment.centerLeft,
+              margin: const EdgeInsets.only(
+                top: 10,
+                left: 22,
+              ),
+              child: const Text(
+                "Features",
+                style: TextStyle(
+                  fontFamily: 'Cera Pro',
+                  color: Pallete.mainFontColor,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            // features list
+             const Column(
+              children: [
+                FeatureList(color: Pallete.firstSuggestionBoxColor,
+                  headerText: 'Genesis-AI',
+                  descriptionText: 'A smarter way to stay organized and informed with Genesis-AI',
+                ),
+                FeatureList(color: Pallete.secondSuggestionBoxColor,
+                  headerText: 'Prince',
+                  descriptionText: 'Get inspired and stay creative with your personal assistant Powered by Prince ',
+                ),
+                FeatureList(color: Pallete.thirdSuggestionBoxColor,
+                  headerText: 'Smart Voice Assistant',
+                  descriptionText: 'Get the best of both worlds with a voice assistant powered by Prince and Genesis-AI',
+
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Pallete.firstSuggestionBoxColor     ,
+        onPressed: () async {
+          if (await speechToText.hasPermission && speechToText.isListening){
+            await startListening();
+          }else if (speechToText.isListening){
+            await stopListening();
+          }else  {
+            initSpeechToText();
+          }
+        },
+      child: const Icon(Icons.mic),
       ),
     );
   }
